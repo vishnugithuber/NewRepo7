05fe9a861e21f1f44c11514c15a67d81ee433ea7
vishnupriya <vishnupriyayadav10@gmail.com>
2022-11-02 15:05:31 +0530

    commited

---
+++ Bankk/Loginpage/Controllers/LoginController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Loginpage.Models;
+
+namespace Loginpage.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public List<UserModel> PutValue()
+        {
+            var users = new List<UserModel>
+            {
+                new UserModel{id=1,username="12345",password="vishnu"},
+                new UserModel{id=2,username="27547",password="password"},
+                new UserModel{id=3,username="2629",password="hiihloo"},
+            };
+
+            return users;
+        }
+
+        [HttpPost]
+        public IActionResult Verify(UserModel usr)
+        {
+            var u = PutValue();
+
+            var ue = u.Where(u => u.username.Equals(usr.username));
+
+            var up = ue.Where(p => p.password.Equals(usr.password));
+
+            if(up.Count()==1)
+            {
+                ViewBag.message = "Login Success";
+                return View("LoginSuccess");
+            }
+            else
+            {
+                ViewBag.message = "Login Failed";
+                return View("Login");
+            }
+        }
+    }
+}
